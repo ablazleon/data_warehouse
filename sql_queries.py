b89bb7078ab58856b4ae44d9b2d925d4806e73bc
ablazleon <ablazleon@gmail.com>
2020-07-05 11:04:29 +0000

    proposed queries

--- sql_queries.py
+++ sql_queries.py
@@ -1,9 +1,23 @@
 import configparser
 
 
+"""
+sql_queries
+
+1. Tables are drop
+2. THen created
+3. Staging tables
+
+"""
+
 # CONFIG
 config = configparser.ConfigParser()
 config.read('dwh.cfg')
+
+ARN             = config.get('IAM_ROLE', 'ARN')
+LOG_DATA        = config.get('S3', 'LOG_DATA')
+LOG_JSONPATH    = config.get('S3', 'LOG_JSONPATH')
+SONG_DATA       = config.get('S3', 'SONG_DATA')
 
 # DROP TABLES
 
@@ -17,40 +31,116 @@ time_table_drop = "DROP TABLE IF EXISTS time;"
 
 # CREATE TABLES
 
-staging_events_table_create= ("""
+staging_events_table_create = ("""CREATE TABLE IF NOT EXISTS stagingevents (
+                event_id    BIGINT IDENTITY(0,1)    NOT NULL,
+                artist      VARCHAR                 NULL,
+                auth        VARCHAR                 NULL,
+                firstName   VARCHAR                 NULL,
+                gender      VARCHAR                 NULL,
+                itemInSession VARCHAR               NULL,
+                lastName    VARCHAR                 NULL,
+                length      VARCHAR                 NULL,
+                level       VARCHAR                 NULL,
+                location    VARCHAR                 NULL,
+                method      VARCHAR                 NULL,
+                page        VARCHAR                 NULL,
+                registration VARCHAR                NULL,
+                sessionId   INTEGER                 NOT NULL SORTKEY DISTKEY,
+                song        VARCHAR                 NULL,
+                status      INTEGER                 NULL,
+                ts          BIGINT                  NOT NULL,
+                userAgent   VARCHAR                 NULL,
+                userId      INTEGER                 NULL);
 
 """)
 
-staging_songs_table_create = ("""
-""")
+staging_songs_table_create = ("""CREATE TABLE IF NOT EXISTS stagingsongs (
+                num_songs           INTEGER         NULL,
+                artist_id           VARCHAR         NOT NULL SORTKEY DISTKEY,
+                artist_latitude     VARCHAR         NULL,
+                artist_longitude    VARCHAR         NULL,
+                artist_location     VARCHAR(500)   NULL,
+                artist_name         VARCHAR(500)   NULL,
+                song_id             VARCHAR         NOT NULL,
+                title               VARCHAR(500)   NULL,
+                duration            DECIMAL(9)      NULL,
+                year                INTEGER         NULL);""")
 
-songplay_table_create = ("""
-""")
 
-user_table_create = ("""
-""")
+user_table_create = (""" CREATE TABLE IF NOT EXISTS users (
+                user_id     INTEGER                 NOT NULL SORTKEY,
+                first_name  VARCHAR(50)             NULL,
+                last_name   VARCHAR(80)             NULL,
+                gender      VARCHAR(10)             NULL,
+                level       VARCHAR(10)             NULL
+    ) diststyle all;""")
 
-song_table_create = ("""
-""")
+song_table_create = ("""CREATE TABLE IF NOT EXISTS songs (
+                song_id     VARCHAR(50)             NOT NULL SORTKEY,
+                title       VARCHAR(500)           NOT NULL,
+                artist_id   VARCHAR(50)             NOT NULL,
+                year        INTEGER                 NOT NULL,
+                duration    DECIMAL(9)              NOT NULL
+    );""")
 
-artist_table_create = ("""
-""")
+artist_table_create = ("""CREATE TABLE IF NOT EXISTS artists (
+                artist_id   VARCHAR(50)             NOT NULL SORTKEY,
+                name        VARCHAR(500)           NULL,
+                location    VARCHAR(500)           NULL,
+                latitude    DECIMAL(9)              NULL,
+                longitude   DECIMAL(9)              NULL
+    ) diststyle all;""")
 
-time_table_create = ("""
-""")
+time_table_create = ("""CREATE TABLE IF NOT EXISTS time (
+                start_time  TIMESTAMP               NOT NULL SORTKEY,
+                hour        SMALLINT                NULL,
+                day         SMALLINT                NULL,
+                week        SMALLINT                NULL,
+                month       SMALLINT                NULL,
+                year        SMALLINT                NULL,
+                weekday     SMALLINT                NULL
+    ) diststyle all;""")
 
 # STAGING TABLES
 
-staging_events_copy = ("""
-""").format()
+staging_events_copy = ("""COPY stagingevents FROM {}
+    credentials 'aws_iam_role={}'
+    format as json {}
+    STATUPDATE ON
+    region 'us-west-2';
+""").format(LOG_DATA, ARN, LOG_JSONPATH)
 
 staging_songs_copy = ("""
-""").format()
+    COPY stagingsongs FROM {}
+    credentials 'aws_iam_role={}'
+    format as json 'auto'
+    ACCEPTINVCHARS AS '^'
+    STATUPDATE ON
+    region 'us-west-2';
+""").format(SONG_DATA, ARN)
 
 # FINAL TABLES
 
-songplay_table_insert = ("""
-""")
+songplay_table_insert = ("""INSERT INTO songplays (start_time,
+                                        user_id,
+                                        level,
+                                        song_id,
+                                        artist_id,
+                                        session_id,
+                                        location,
+                                        user_agent) SELECT  DISTINCT TIMESTAMP 'epoch' + se.ts/1000 \
+                * INTERVAL '1 second'   AS start_time,
+            se.userId                   AS user_id,
+            se.level                    AS level,
+            ss.song_id                  AS song_id,
+            ss.artist_id                AS artist_id,
+            se.sessionId                AS session_id,
+            se.location                 AS location,
+            se.userAgent                AS user_agent
+    FROM stagingevents AS se
+    JOIN stagingsongs AS ss
+        ON (se.artist = ss.artist_name)
+    WHERE se.page = 'NextSong';""")
 
 user_table_insert = ("""
 """)
